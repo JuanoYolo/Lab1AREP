620455734bf439e043bcea91a4ed0b7a4bda102f
unknown <juan.monroy-s@labinfo.is.escuelaing.edu.co>
2022-08-12 14:03:22 -0500

    Parte Inicial Lab

---
+++ src/main/java/co/edu/escuelaing/stockwebapp/StockWebApp.java
@@ -0,0 +1,26 @@
+package co.edu.escuelaing.stockwebapp;
+
+import static spark.Spark.*;
+
+public class StockWebApp {
+    public static void main(String[] args) {
+        port(getPort());
+        staticFiles.location("/public");
+        get("/hello", (req, res) -> "Hello heroku" + req.queryParams("name"));
+
+        get("/stock", (req, res) -> {
+            res.type("application/json");
+            return HttpConnectionExample.getStock();
+        });
+
+
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)
+
+    }
+}
